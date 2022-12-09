f8ad99f96d210246f4b434ba230ba678200dea0f
atakanoguz <atakan17171717@hotmail.com>
2022-12-09 17:13:10 +0300

    message controller class added

--- backend/src/main/java/com/beyda/encryptmessageservice/controller/MessageController.java
+++ backend/src/main/java/com/beyda/encryptmessageservice/controller/MessageController.java
@@ -4,9 +4,7 @@ import com.beyda.encryptmessageservice.model.Message;
 import com.beyda.encryptmessageservice.service.MessageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,7 +15,9 @@ public class MessageController {
 
     private final MessageService messageService;
 
-    public ResponseEntity<List<Message>> getMassege(@PathVariable String room){
-        return ResponseEntity.ok(messageService.getMessage(room));
+    @CrossOrigin
+    @GetMapping("/{room}")
+    public ResponseEntity<List<Message>> getMessages(@PathVariable String room) {
+        return ResponseEntity.ok(messageService.getMessages(room));
     }
 }
