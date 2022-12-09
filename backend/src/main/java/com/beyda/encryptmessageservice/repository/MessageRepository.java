83bba91cab3c62b6585a9ac36815a2b392e1e08b
atakanoguz <atakan17171717@hotmail.com>
2022-12-09 16:56:26 +0300

    package.json

---
+++ backend/src/main/java/com/beyda/encryptmessageservice/repository/MessageRepository.java
@@ -0,0 +1,13 @@
+package com.beyda.encryptmessageservice.repository;
+
+import com.beyda.encryptmessageservice.model.Message;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface MessageRepository extends JpaRepository<Message, Long> {
+
+    List<Message> findAllByRoom(String room);
+}
--- backend/src/main/java/com/beyda/encryptmessageservice/service/MessageService.java
+++ backend/src/main/java/com/beyda/encryptmessageservice/service/MessageService.java
@@ -1,4 +1,14 @@
 package com.beyda.encryptmessageservice.service;
 
+import com.beyda.encryptmessageservice.repository.MessageRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
 public class MessageService {
+
+    private final MessageRepository messageRepository;
 }
