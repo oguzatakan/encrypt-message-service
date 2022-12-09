231d2a346f063a7146b4f945bddb4356f6f81c01
atakanoguz <atakan17171717@hotmail.com>
2022-12-09 17:13:23 +0300

    message service class added

--- backend/src/main/java/com/beyda/encryptmessageservice/service/MessageService.java
+++ backend/src/main/java/com/beyda/encryptmessageservice/service/MessageService.java
@@ -1,9 +1,12 @@
 package com.beyda.encryptmessageservice.service;
 
+import com.beyda.encryptmessageservice.model.Message;
 import com.beyda.encryptmessageservice.repository.MessageRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @Slf4j
@@ -11,4 +14,13 @@ import org.springframework.stereotype.Service;
 public class MessageService {
 
     private final MessageRepository messageRepository;
+
+    public List<Message> getMessages(String room) {
+        return messageRepository.findAllByRoom(room);
+    }
+
+    public Message saveMessage(Message message) {
+        return messageRepository.save(message);
+    }
+
 }
