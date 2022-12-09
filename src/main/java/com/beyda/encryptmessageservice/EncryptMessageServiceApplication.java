bee3a4359ef905661377cb6a82ed049358ddaa8e
atakanoguz <atakan17171717@hotmail.com>
2022-12-09 11:11:44 +0300

    basicupdates

--- src/main/java/com/beyda/encryptmessageservice/EncryptMessageServiceApplication.java
+++ src/main/java/com/beyda/encryptmessageservice/EncryptMessageServiceApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EncryptMessageServiceApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(EncryptMessageServiceApplication.class, args);
 	}
