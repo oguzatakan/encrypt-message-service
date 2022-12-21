10a20b0dc9ec9a25a2abcb222ce7d01ee23a5d84
atakanoguz <atakan17171717@hotmail.com>
2022-12-21 17:49:09 +0300

    api url added

--- frontend/src/constants/apiConstants.js
+++ frontend/src/constants/apiConstants.js
@@ -1,2 +1,2 @@
-export const SOCKET_BASE_URL = "http://192.168.1.6:8085";
-export const API_BASE_URL = "http://192.168.1.6:8080";
+export const SOCKET_BASE_URL = "http://192.168.0.10:8085";
+export const API_BASE_URL = "http://192.168.0.10:8080";
